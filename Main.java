98a260e7277e133e4d8d1f2bc6c7298723ac44b6
kowsikadevi21082006 <deva36rp@gamil.com>
2024-08-04 19:40:31 +0530

    Added array and objects

--- Main.java
+++ Main.java
@@ -25,6 +25,11 @@ class PetCare {
     private Pet[] pets;
     private int numPets;
 
+    public PetCare() {
+        pets = new Pet[MAX_PETS];
+        numPets = 0;
+    }
+
     public void getData() {
         Scanner scanner = new Scanner(System.in);
 
