39d2246c31ff3157fde00b3a4bb0ae58ea2ac63b
kowsikadevi21082006 <deva36rp@gamil.com>
2024-11-22 10:49:36 +0530

    done

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+// Abstract base class representing a Pet
 abstract class Pet {
     private static int totalPets = 0;
     private int id;
@@ -7,27 +8,16 @@ abstract class Pet {
     private int age;
     private int hungerLevel;
 
-    // Default Constructor
-    public Pet() {
-        this.id = 0;
-        this.name = "Unknown";
-        this.age = 0;
-        this.hungerLevel = 0;
-        totalPets++;
-        System.out.println("Default Constructor called for Pet");
-    }
-
-    // Parameterized Constructor (Constructor Overloading)
+    // Constructor to initialize Pet attributes
     public Pet(int id, String name, int age, int hungerLevel) {
         this.id = id;
         this.name = name;
         this.age = age;
         this.hungerLevel = hungerLevel;
         totalPets++;
-        System.out.println("Parameterized Constructor called for Pet");
     }
 
-    // Getter and Setter Methods
+    // Getters
     public int getId() {
         return id;
     }
@@ -44,43 +34,23 @@ abstract class Pet {
         return hungerLevel;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setHungerLevel(int hungerLevel) {
-        this.hungerLevel = hungerLevel;
-    }
-
+    // Static method to get total number of pets created
     public static int getTotalPets() {
         return totalPets;
     }
 
+    // Static method to reset the total count of pets
     public static void resetTotalPets() {
         totalPets = 0;
     }
 
-    // Abstract methods (Virtual Functions in Java are dynamically bound)
-    public abstract String getType(); // Overridden in subclasses (Method Overriding)
-    public abstract void display();   // Overridden in subclasses (Method Overriding)
+    // Abstract methods to be implemented by subclasses
+    public abstract String getType();
+    public abstract void display();
 }
 
-// Single Inheritance: Dog inherits from Pet
+// Class representing a Dog (inherits from Pet)
 class Dog extends Pet {
-    // Using Default Constructor
-    public Dog() {
-        super();
-    }
-
-    // Using Parameterized Constructor
     public Dog(int id, String name, int age, int hungerLevel) {
         super(id, name, age, hungerLevel);
     }
@@ -92,18 +62,12 @@ class Dog extends Pet {
 
     @Override
     public void display() {
-        System.out.println(getId() + "\t\t" + getName() + "\t\t" + getType() + "\t\t" + getAge() + "\t\t" + getHungerLevel());
+        System.out.printf("%d\t%s\t%s\t%d\t%d%n", getId(), getName(), getType(), getAge(), getHungerLevel());
     }
 }
 
-// Hierarchical Inheritance: Cat inherits from Pet
+// Class representing a Cat (inherits from Pet)
 class Cat extends Pet {
-    // Using Default Constructor
-    public Cat() {
-        super();
-    }
-
-    // Using Parameterized Constructor
     public Cat(int id, String name, int age, int hungerLevel) {
         super(id, name, age, hungerLevel);
     }
@@ -115,18 +79,12 @@ class Cat extends Pet {
 
     @Override
     public void display() {
-        System.out.println(getId() + "\t\t" + getName() + "\t\t" + getType() + "\t\t" + getAge() + "\t\t" + getHungerLevel());
+        System.out.printf("%d\t%s\t%s\t%d\t%d%n", getId(), getName(), getType(), getAge(), getHungerLevel());
     }
 }
 
-// Hierarchical Inheritance: Bird inherits from Pet
+// Class representing a Bird (inherits from Pet)
 class Bird extends Pet {
-    // Using Default Constructor
-    public Bird() {
-        super();
-    }
-
-    // Using Parameterized Constructor
     public Bird(int id, String name, int age, int hungerLevel) {
         super(id, name, age, hungerLevel);
     }
@@ -138,21 +96,14 @@ class Bird extends Pet {
 
     @Override
     public void display() {
-        System.out.println(getId() + "\t\t" + getName() + "\t\t" + getType() + "\t\t" + getAge() + "\t\t" + getHungerLevel());
+        System.out.printf("%d\t%s\t%s\t%d\t%d%n", getId(), getName(), getType(), getAge(), getHungerLevel());
     }
 }
 
-// Multi-Level Inheritance: GuideDog inherits from Dog
+// Class representing a GuideDog (inherits from Dog)
 class GuideDog extends Dog {
     private String serviceType;
 
-    // Using Default Constructor
-    public GuideDog() {
-        super();
-        this.serviceType = "Unknown";
-    }
-
-    // Using Parameterized Constructor (Constructor Overloading)
     public GuideDog(int id, String name, int age, int hungerLevel, String serviceType) {
         super(id, name, age, hungerLevel);
         this.serviceType = serviceType;
@@ -162,20 +113,17 @@ class GuideDog extends Dog {
         return serviceType;
     }
 
-    public void setServiceType(String serviceType) {
-        this.serviceType = serviceType;
-    }
-
     @Override
     public void display() {
         super.display();
-        System.out.println("\t\t" + "Service Type: " + getServiceType());
+        System.out.printf("\tService Type: %s%n", getServiceType());
     }
 }
 
+// Class to manage Pet operations
 class PetCare {
     private static final int MAX_PETS = 100;
-    private Pet[] pets; 
+    private Pet[] pets;
     private int numPets;
 
     public PetCare() {
@@ -183,15 +131,16 @@ class PetCare {
         numPets = 0;
     }
 
+    // Method to collect pet data
     public void getData() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the number of pets you want to store in PetCare: ");
+        System.out.print("Enter the number of pets: ");
         numPets = scanner.nextInt();
         scanner.nextLine();
 
         if (numPets > MAX_PETS) {
-            System.out.println("The number of pets exceeds the limit.");
+            System.out.println("Exceeded max pets. Storing only " + MAX_PETS + " pets.");
             numPets = MAX_PETS;
         }
 
@@ -199,48 +148,54 @@ class PetCare {
             System.out.print("Enter Pet Type (Dog/Cat/Bird/GuideDog): ");
             String type = scanner.nextLine();
 
-            System.out.print("Enter Pet ID: ");
+            System.out.print("Enter ID: ");
             int id = scanner.nextInt();
             scanner.nextLine();
 
-            System.out.print("Enter Pet Name: ");
+            System.out.print("Enter Name: ");
             String name = scanner.nextLine();
 
-            System.out.print("Enter Pet Age: ");
+            System.out.print("Enter Age: ");
             int age = scanner.nextInt();
             scanner.nextLine();
 
-            System.out.print("Enter Pet Hunger Level: ");
+            System.out.print("Enter Hunger Level: ");
             int hungerLevel = scanner.nextInt();
             scanner.nextLine();
 
-            if (type.equalsIgnoreCase("Dog")) {
-                pets[i] = new Dog(id, name, age, hungerLevel);
-            } else if (type.equalsIgnoreCase("Cat")) {
-                pets[i] = new Cat(id, name, age, hungerLevel);
-            } else if (type.equalsIgnoreCase("Bird")) {
-                pets[i] = new Bird(id, name, age, hungerLevel);
-            } else if (type.equalsIgnoreCase("GuideDog")) {
-                System.out.print("Enter Service Type for GuideDog: ");
-                String serviceType = scanner.nextLine();
-                pets[i] = new GuideDog(id, name, age, hungerLevel, serviceType);
-            } else {
-                System.out.println("Unknown pet type! Defaulting to Dog.");
-                pets[i] = new Dog(id, name, age, hungerLevel); 
+            switch (type.toLowerCase()) {
+                case "dog":
+                    pets[i] = new Dog(id, name, age, hungerLevel);
+                    break;
+                case "cat":
+                    pets[i] = new Cat(id, name, age, hungerLevel);
+                    break;
+                case "bird":
+                    pets[i] = new Bird(id, name, age, hungerLevel);
+                    break;
+                case "guidedog":
+                    System.out.print("Enter Service Type: ");
+                    String serviceType = scanner.nextLine();
+                    pets[i] = new GuideDog(id, name, age, hungerLevel, serviceType);
+                    break;
+                default:
+                    System.out.println("Unknown type, defaulting to Dog.");
+                    pets[i] = new Dog(id, name, age, hungerLevel);
+                    break;
             }
         }
     }
 
+    // Method to display pet data
     public void display() {
-        System.out.println("PetCare Pets");
-        System.out.println("ID \t\t Name \t\t Type \t\t Age \t\t Hunger Level");
+        System.out.println("ID\tName\tType\tAge\tHunger Level");
         for (int i = 0; i < numPets; i++) {
-            pets[i].display(); 
+            pets[i].display();
         }
-        System.out.println();
-        System.out.println("Total Pets Created: " + Pet.getTotalPets()); 
+        System.out.println("\nTotal Pets Created: " + Pet.getTotalPets());
     }
 
+    // Method to reset pet data
     public void resetPetsData() {
         Pet.resetTotalPets();
         numPets = 0;
@@ -248,13 +203,13 @@ class PetCare {
     }
 }
 
+// Main class
 public class Main {
     public static void main(String[] args) {
         PetCare petCare = new PetCare();
-        petCare.getData();  
-        petCare.display(); 
-        
-        petCare.resetPetsData();  
+        petCare.getData();
+        petCare.display();
+        petCare.resetPetsData();
         petCare.display();
     }
 }
